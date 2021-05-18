b4f084cb74dcfe23775c8d7c2974a15d7eb081ce
Vikash Kumar <35370115+vikash-kumar01@users.noreply.github.com>
2021-05-18 16:50:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
-# Configure the AWS Provide
 provider "aws" {
-region = "ap-south-1"
+  region     = "us-east-1"
+  access_key = "AKIASYIIRCL7UKMUYKOT"
+  secret_key = "PSDvSaJdkeKkOCICVeoBY118Jv3zdoW/VNMHA6wz"
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+ 
+
+resource "aws_instance" "web" {
+  ami           = "ami-0b9064170e32bde34"
+  instance_type = "t2.micro"
 }
