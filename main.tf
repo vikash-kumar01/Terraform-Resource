e4b0c7eda12f984011f49181d7b938e043fff4f0
Vikash Kumar <35370115+vikash-kumar01@users.noreply.github.com>
2021-05-18 16:31:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-2"
+   access_key = "AKIASYIIRCL7UKMUYKOT"
+  secret_key = "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"
 }
 
 # Create a VPC
